3028d7e75287477855b9a3f4c495e637ac2a3a1c
Silky <55907293+ksilky@users.noreply.github.com>
2022-01-30 15:12:26 +0530

    add abs to rmse function

--- assignmentImageDenoising/code/rrmse.m
+++ assignmentImageDenoising/code/rrmse.m
@@ -1,5 +1,4 @@
 function [out] = rrmse(x1,x2)
 % outputs the relative mean squared error wrt x2
-out = sqrt(sum(mean((x1-x2).^2),'all'))/sqrt(sum(mean((x2).^2),'all'))
+out = sqrt(sum(mean((abs(x1)-abs(x2)).^2),'all'))/sqrt(sum(mean((x1).^2),'all'))
 end
-
