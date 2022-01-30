e13ccd26491d0dfb632afb758c293f1133c528d8
Silky <55907293+ksilky@users.noreply.github.com>
2022-01-30 15:14:46 +0530

    plot objective function value vs iteration

--- assignmentImageDenoising/code/code.m
+++ assignmentImageDenoising/code/code.m
@@ -11,14 +11,14 @@ clear;
 % 1 - Quadratic Prior
 % 2 - Huber Prior
 % 3 - The other prior
-prior = 1;
+prior = 2;
 
 % 1 - Gaussian Noise
 % 2 - Rician Noise
 noise = 2;
 
 % Weighting term between noise and likelihood
-alpha = 0.00001;
+alpha = 1-0.00001;
 
 A = importdata('../data/assignmentImageDenoisingPhantom.mat');
 clean = A.imageNoiseless;
@@ -34,9 +34,9 @@ x_old = y;
 prev_delta = 0;
 
 itc = 0;
-obj = [];
+objective_func_value = [];
 
-while 0      
+while 1     
     diff = zeros(4,m,n);
     diff(1,:,:) = x_new-circshift(x_new,[0 -1]);
     diff(2,:,:) = x_new-circshift(x_new,[ 0 1]);
@@ -47,22 +47,25 @@ while 0
     reg_d = zeros(m,n);
     thr = 0;
     step = 0;
+    gamma=0;
     for idx = 1:4
         if prior == 1
             % Quadratic MRF Prior
             reg_d = reg_d + squeeze(2*diff(idx,:,:));
-            thr = 2e-4;
+            thr = 1e-3;
             step = 0.01;
             beta = 0.5;
         elseif prior == 2
             % Huber MRF Prior
-            reg_d = reg_d + squeeze(huber_der(squeeze(diff(idx,:,:)), 0.005));
+            gamma = 0.005;
+            reg_d = reg_d + squeeze(huber_der(squeeze(diff(idx,:,:)), gamma));
             thr = 1e-5;
             step = 0.001;
             beta = 0.5;     % Gradient Descent with momentum
         elseif prior == 3
             % The custom function's prior
-            reg_d = reg_d + squeeze(disc_adap_der(squeeze(diff(idx,:,:)), 0.1));
+            gamma = 0.1;
+            reg_d = reg_d + squeeze(disc_adap_der(squeeze(diff(idx,:,:)), gamma));
             thr = 1e-4;
             step = 0.001;
             beta = 0.5;
@@ -73,32 +76,42 @@ while 0
     itc = itc + 1;
     if noise == 1
         % Using gaussian noise
-        fid = 2*(x_new-y)*(alpha);        
+        fid = 2*(x_new-y)*(1-alpha);        
     elseif noise == 2
         % Using Rician Noise
-        arg = (x_new.*y)*(alpha);
-        fid = (alpha)*(x_new - (besseli(1,arg)./besseli(0,arg)).*(y));
+        arg = (x_new.*y)*(1-alpha);
+        fid = (1-alpha)*(x_new - (besseli(1,arg)./besseli(0,arg)).*(y));
         
     end
         
     
-    der = fid + reg_d*(1-alpha);
+    der = fid + reg_d*(alpha);
     x_old = x_new;
     x_new = x_new - (step*der + beta*prev_delta);
     prev_delta = (step*der + beta*prev_delta);
     
-    
+    objective_func_value(itc)=objective_function(x_new,y,diff,alpha,gamma,prior,noise);
     rrmse(x_new, x_old)
     if rrmse(x_new, x_old) < thr
         break
     end
     
 end
+figure;
+subplot(1,3,1), imshow(x_new);
+title('noisless image', 'FontSize', 15);
+subplot(1,3,2), imshow(y);
+title('noisy image', 'FontSize', 15);
+subplot(1,3,3), imshow(clean);
+title('clean image', 'FontSize', 15);
 
-%subplot(1,3,1), imshow(x_new);
-%subplot(1,3,2), imshow(y);
-%subplot(1,3,3), imshow(clean);
 
+iterations= linspace(1,itc,itc);
+figure;
+plot(iterations,objective_func_value);
+xlabel('iteration');
+ylabel('objective function value');
+title('objective function value vs iteration');
 
 post_diff = rrmse(x_new, clean);
 init_diff = rrmse(y, clean);
