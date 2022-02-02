8d2a3866825d64190622f8cc505d8a5116d61afa
Akash Cherukuri <190050009@iitb.ac.in>
2022-02-02 17:35:01 +0530

    not a big deal

--- assignmentImageDenoising/code/rrmse.m
+++ assignmentImageDenoising/code/rrmse.m
@@ -1,4 +1,4 @@
 function [out] = rrmse(x1,x2)
 % outputs the relative mean squared error wrt x2
-out = sqrt(sum(mean((abs(x1)-abs(x2)).^2),'all'))/sqrt(sum(mean((x1).^2),'all'))
+out = sqrt(sum(mean((abs(x1)-abs(x2)).^2),'all'))/sqrt(sum(mean((x1).^2),'all'));
 end
