8b6b559e2d80dabd64b859d549dd380f14e96588
ksilky <ksilky747@gmail.com>
2022-03-27 22:30:02 +0530

    code2 completed

--- assignmentShapeAnalysis/code/code.m
+++ assignmentShapeAnalysis/code/code.m
@@ -12,3 +12,14 @@ sgtitle('initial poinset');
 modes(nps,np,aligned_points1,shape_mean1);
 [aligned_points2,shape_mean2]= code22(nps,np,points); 
 modes(nps,np,aligned_points2,shape_mean2);
+
+plotter(nps,aligned_points1);
+sgtitle('Aligned pointset1');
+figure;
+plot(shape_mean1(1,:),shape_mean1(2,:) ,'-*r'); 
+sgtitle('Shape mean1');
+plotter(nps,aligned_points2);
+sgtitle('Aligned pointset2');
+figure;
+plot(shape_mean2(1,:),shape_mean2(2,:) ,'-*r');  
+sgtitle('Shape mean2');
